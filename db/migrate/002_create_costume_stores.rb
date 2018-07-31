5f2cdd6e9e0d2826f67cf9203911d4a14928f78c
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-07-31 11:12:19 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean :open?
+      t.boolean :still_in_business
       t.integer :opening
       t.integer :closing
     end
